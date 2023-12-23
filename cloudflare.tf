e3a0080347edef93a6493ec01a0c0b270b05aed9
aliasboink <adrianlaurb@gmail.com>
2023-12-23 17:51:21 +0200

    No more Cloudflare records

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,23 +1,3 @@
 data "cloudflare_ip_ranges" "cloudflare" {}
 
-resource "cloudflare_record" "subdomain" {
-  zone_id         = var.cloudflare_zone_id
-  name            = "www"
-  value           = aws_s3_bucket_website_configuration.subdomain.website_endpoint
-  type            = "CNAME"
-  ttl             = 1
-  allow_overwrite = false
-  comment         = "This is a necessary value otherwise the Cloudflare TF Provider cries."
-  proxied         = true
-}
-
-resource "cloudflare_record" "apex" {
-  zone_id         = var.cloudflare_zone_id
-  name            = "adrian-docs.com"
-  value           = aws_s3_bucket_website_configuration.apex.website_endpoint
-  type            = "CNAME"
-  ttl             = 1
-  allow_overwrite = false
-  comment         = "This is a necessary value otherwise the Cloudflare TF Provider cries."
-  proxied         = true
-}
+# I give up on the records. The provider is too borked.
